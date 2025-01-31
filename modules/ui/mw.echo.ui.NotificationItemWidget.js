87f77d96cda44f885d00aaa38d48771acd5fed73
Jayden Bailey <jaydenkieran@gmail.com>
2025-01-31 14:58:19 +0000

    Handle external links with target=_blank and mark as read on click

--- modules/ui/mw.echo.ui.NotificationItemWidget.js
+++ modules/ui/mw.echo.ui.NotificationItemWidget.js
@@ -210,6 +210,13 @@
 				.attr( 'href', this.model.getPrimaryUrl() )
 				.on( 'click', this.onPrimaryLinkClick.bind( this ) )
 				.on( 'auxclick', this.onPrimaryLinkAuxclick.bind( this ) );
+
+			// WGL change - if the notification's link is external, handle it differently
+			if ( !this.model.getPrimaryUrl().startsWith( mw.config.get('wgServer') ) ) {
+				this.$element
+					.addClass( 'mw-echo-ui-notificationItemWidget-external' )
+					.attr( 'target', '_blank' );
+			}
 		}
 	};
 
@@ -221,10 +228,16 @@
 	/**
 	 * Respond to primary link click.
 	 * Override this in the descendents.
-	 *
+	 **
 	 * @return {boolean} true
 	 */
-	mw.echo.ui.NotificationItemWidget.prototype.onPrimaryLinkClick = function () {
+	mw.echo.ui.NotificationItemWidget.prototype.onPrimaryLinkClick = function ( ) {
+		// WGL change - if the notification's link is external, handle it differently
+		if ( this.$element.hasClass( 'mw-echo-ui-notificationItemWidget-external' ) ) {
+			this.$element.removeClass( 'mw-echo-ui-notificationItemWidget-initiallyUnseen' );
+			this.markRead( true );
+		}
+
 		return true;
 	};
 
